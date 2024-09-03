67a390a5fb0297093a016538bd5bc43a420e218b
Larissa Rabelo <larissarabelolf@gmail.com>
2024-09-02 23:35:36 -0300

    feat: add seed

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -8,8 +8,9 @@ const prisma = new PrismaClient();
 async function main() {
   console.log('Start seeding...');
 
-  const user = await createRootUser();
-  await seedEquipments(user.id);
+  const { id } = await createRootUser();
+  const equipmentsIds = await seedEquipments(id);
+  await seedMeasurements(equipmentsIds);
 
   console.log('Finished seeding.');
 }
@@ -24,6 +25,14 @@ main()
   });
 
 async function createRootUser(): Promise<{ id: string }> {
+  const existingUser = await prisma.user.findUnique({
+    where: { email: 'root@example.com' },
+  });
+
+  if (existingUser) {
+    return { id: existingUser.id };
+  }
+
   return (await prisma.user.create({
     data: {
       email: 'root@example.com',
@@ -37,17 +46,82 @@ async function seedEquipments(userId: string) {
   const filePath = path.join(__dirname, 'seed', 'equipments.json');
   const rawData = fs.readFileSync(filePath, 'utf-8');
   const equipmentData = JSON.parse(rawData);
+  const equipmentsIds: string[] = [];
 
   for (const equipment of equipmentData) {
-    await prisma.equipment.create({
+    const existingEquipment = await prisma.equipment.findUnique({
+      where: { id: equipment.id },
+    });
+
+    if (!existingEquipment) {
+      const equip = await prisma.equipment.create({
+        data: {
+          id: equipment.id,
+          name: equipment.name,
+          description: equipment.description,
+          userId: userId,
+        },
+      });
+
+      equipmentsIds.push(equip.id);
+    }
+  }
+
+  console.log('Equipments created successfully.');
+
+  return equipmentsIds;
+}
+
+async function seedMeasurements(equipmentIds: string[]) {
+  const measurements = generateMeasurements(1000, equipmentIds);
+
+  for (const measurement of measurements) {
+    await prisma.measurement.create({
       data: {
-        id: equipment.id,
-        name: equipment.name,
-        description: equipment.description,
-        userId: userId,
+        equipmentId: measurement.equipmentId,
+        timestamp: new Date(measurement.timestamp),
+        value: measurement.value,
       },
     });
   }
 
-  console.log('Equipments created successfully.');
+  console.log('Measurements created successfully.');
 }
+
+const getRandomDate = (start: Date, end: Date): Date => {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime()),
+  );
+};
+
+const getRandomValue = (): number => {
+  return parseFloat((Math.random() * 100).toFixed(2));
+};
+
+const generateMeasurements = (
+  numMeasurements: number,
+  equipmentIds: string[],
+): { equipmentId: string; timestamp: string; value: number }[] => {
+  const measurements: Measurement[] = [];
+  const startDate = new Date();
+  startDate.setMonth(startDate.getMonth() - 3); // 3 meses atrás
+  const endDate = new Date(); // Hoje
+
+  for (let i = 0; i < numMeasurements; i++) {
+    const measurement: Measurement = {
+      equipmentId:
+        equipmentIds[Math.floor(Math.random() * equipmentIds.length)] ?? '',
+      timestamp: getRandomDate(startDate, endDate).toISOString(),
+      value: getRandomValue(),
+    };
+    measurements.push(measurement);
+  }
+
+  return measurements;
+};
+
+type Measurement = {
+  equipmentId: string;
+  timestamp: string;
+  value: number;
+};
